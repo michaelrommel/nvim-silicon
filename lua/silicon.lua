f67aae0b5fa509e902cd88e68086cea19abf756f
Michael Rommel <rommel@layer-7.net>
2023-05-03 20:58:10 +0200

    feat: add option template

--- lua/silicon.lua
+++ lua/silicon.lua
@@ -1,12 +1,47 @@
 local M = {}
 
+M.default_opts = {
+	font = "VictorMono-NF=34;Noto Emoji=34;",
+	theme = "gruvbox-dark",
+	background = "#076678",
+	background_image = "",
+	pad_horiz = 100,
+	pad_vert = 80,
+	no_round_corner = false,
+	no_window_controls = false,
+	no_line_number = false,
+	line_offset = 1,
+	line_pad = 0,
+	tab_width = 4,
+	gobble = true,
+	highlight_lines = true,
+	shadow = {
+		blur_radius = 16,
+		offset_x = 8,
+		offset_y = 8,
+		color = "#100808"
+	},
+	output = function()
+		return "./" .. os.date("!%Y-%m-%dT%H:%M:%S") .. "_screenshot.png"
+	end,
+	command = "silicon"
+}
+
 M.start = function()
 	print("Silicon started")
+	for 
+	
 end
 
 M.setup = function(opts)
 	vim.validate({
 		opts = { opts, "table" }
+	})
+
+	M.opts = vim.tbl_deep_extend({
+		"force",
+		M.default_opts,
+		opts
 	})
 
 	vim.api.nvim_create_user_command("Silicon", function(_)
