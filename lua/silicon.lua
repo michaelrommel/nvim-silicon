d650874d24b13590fcbde8850ebb3f83b72c34d0
Michael Rommel <rommel@layer-7.net>
2023-05-04 02:41:23 +0200

    feat: first working version

--- lua/silicon.lua
+++ lua/silicon.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.default_opts = {
-	font = "VictorMono-NF=34;Noto Emoji=34;",
+	font = "VictorMono Nerd Font=34;Noto Emoji",
 	theme = "gruvbox-dark",
 	background = "#076678",
 	background_image = nil,
@@ -13,27 +13,69 @@ M.default_opts = {
 	line_offset = 1,
 	line_pad = 0,
 	tab_width = 4,
-	gobble = true,
-	highlight_lines = true,
+	highlight_lines = nil,
+	language = function()
+		return vim.bo.filetype
+	end,
 	shadow_blur_radius = 16,
 	shadow_offset_x = 8,
 	shadow_offset_y = 8,
 	shadow_color = "#100808",
 	output = function()
-		return "./" .. os.date("!%Y-%m-%dT%H:%M:%S") .. "_screenshot.png"
+		return "./" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
 	end,
 	command = "silicon"
 }
 
-M.start = function()
+M.start = function(args)
 	print("Silicon started")
-	local args = ""
+	local cmdline = {}
+	table.insert(cmdline, M.opts.command)
 	for k, v in pairs(M.opts) do
-		if k ~= "command" then
-			args = args .. " --" .. string.gsub(k, "_", "-")
+		if k == "command" then
+			-- no-op
+		elseif k == "language" then
+			table.insert(cmdline, '--language')
+			if type(v) == "function" then
+				table.insert(cmdline, v())
+			else
+				table.insert(cmdline, v)
+			end
+		elseif k == "output" then
+			table.insert(cmdline, '--output')
+			if type(v) == "function" then
+				table.insert(cmdline, v())
+			else
+				table.insert(cmdline, v)
+			end
+		else
+			if type(v) == "boolean" then
+				if v then
+					table.insert(cmdline, "--" .. string.gsub(k, "_", "-"))
+				end
+			elseif type(v) == "number" then
+				table.insert(cmdline, "--" .. string.gsub(k, "_", "-"))
+				table.insert(cmdline, v)
+			elseif type(v) == "nil" then
+				-- no-op
+			else
+				table.insert(cmdline, "--" .. string.gsub(k, "_", "-"))
+				table.insert(cmdline, v)
+			end
 		end
 	end
-	print(M.opts.command .. args)
+	local marks = vim.api.nvim_buf_get_mark(vim.api.nvim_win_get_buf(0), "h")[1]
+	if marks > 0 then
+		local hl
+		if args.line1 and marks >= (args.line1 - 1) and marks <= args.line2 then
+			hl = marks - (args.line1 - 1)
+			table.insert(cmdline, "--highlight-lines")
+			table.insert(cmdline, hl)
+		end
+	end
+	local lines = vim.api.nvim_buf_get_lines(vim.api.nvim_win_get_buf(0), args.line1 - 1, args.line2, false)
+	local ret = vim.fn.system(cmdline, lines)
+	print(ret)
 end
 
 M.setup = function(opts)
@@ -41,19 +83,18 @@ M.setup = function(opts)
 		opts = { opts, "table" }
 	})
 
-	M.opts = vim.tbl_deep_extend({
+	M.opts = vim.tbl_deep_extend(
 		"force",
 		M.default_opts,
-		opts
-	})
+		opts or {}
+	)
 
-	vim.api.nvim_create_user_command("Silicon", function(_)
-		M.start()
+	vim.api.nvim_create_user_command("Silicon", function(args)
+		M.start(args)
 	end, {
-		bang = true,
-		desc = "require('silicon').start()",
-		nargs = 0,
-		bar = true,
+		desc = "convert range to code image representation",
+		force = false,
+		range = true
 	})
 end
 
